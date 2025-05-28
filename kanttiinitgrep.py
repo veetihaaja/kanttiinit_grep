f0c934e9f2592e6772ad299b6287a57815ba481e
Veeti Haaja <veeti.haaja1@gmail.com>
2025-05-28 14:32:57 +0300

    rewrote entire algorithm, now supports easy swapping between restaurants (currently supports the three unicafes in kumpula), displays food information (atleast vegan, gluten-free and lactose-free), and checks if the restaurant has no menu listed. Also aborts script if urllib request throws error.

--- kanttiinitgrep.py
+++ kanttiinitgrep.py
@@ -1,32 +1,94 @@
 import urllib.request
+from time import strftime, localtime
+import sys
 
-fp = urllib.request.urlopen("https://folio.kanttiinit.fi/fi/area/7")
-mybytes = fp.read()
+def gethtml(url):
 
-mystr = mybytes.decode("utf8")
-fp.close()
+    try:
+        req = urllib.request.urlopen(url)
+        rawhtml = req.read()
+        htmlstr = rawhtml.decode("utf8")
+        req.close()
+    except:
+        print("Error fetching the URL, stopping script")
+        htmlstr = ""
+        sys.exit()
+    return htmlstr
 
-mystr = mystr.split("<h3>")
+def parseFoodData(htmlstr, restaurantsToDisplay):
+    """
+    Parses the HTML string from the Kanttiinit website and extracts food data for specified restaurants.
+    returns a dictionary with restaurant names as keys and their food items as a string.
+    """
 
-mystr = mystr[1:]
+    #dictionary for final data
+    foodRawData = {i:"" for i in restaurantsToDisplay}
+    foodFinalData = {i:"" for i in restaurantsToDisplay}
 
-ravintolat = ["Chemicum"]
+    # used for parsing
+    urldict = {"Chemicum":"https://unicafe.fi/restaurants/chemicum/",
+               "Exactum":"https://unicafe.fi/restaurants/exactum/",
+               "Chemicum Opettajien ravintola":"https://unicafe.fi/restaurants/chemicum-opettajien-ravintola/"}
+    
+    #split by restaurant header
+    dataList = htmlstr.split("<h3>")
 
-copystr = ""
+    #get desired restaurants
+    for data in dataList:
+        for restaurant in restaurantsToDisplay:
+            if urldict[restaurant] in data:
+                foodRawData[restaurant]=data
+    
+    #check for "ei ruokalistaa"
+    for restaurant in foodRawData.keys():
+        if "Ei ruokalistaa." in foodRawData[restaurant]:
+            foodFinalData[restaurant] = "Ei ruokalistaa."
+            foodRawData[restaurant] = ""
 
-for i, val in enumerate(mystr):
-    if val[0:8] in ravintolat:
-        copystr = mystr[i]
-        break
+    #split by list item:
+    for restaurant in foodRawData.keys():
+        if foodRawData[restaurant] != "":
+            foodRawData[restaurant] = foodRawData[restaurant].split("<li>")
+            foodRawData[restaurant] = foodRawData[restaurant][1:]
 
-copystr = copystr.split("<li>")
+    for restaurant in foodRawData.keys():
+        if foodRawData[restaurant] != "":
+            for i in range(len(foodRawData[restaurant])):
+                foodRawData[restaurant][i] = foodRawData[restaurant][i].split("(")
+                foodRawData[restaurant][i][0] = foodRawData[restaurant][i][0].strip()
 
+    for restaurant in foodRawData.keys():
+        if foodRawData[restaurant] != "":
+            for i in range(len(foodRawData[restaurant])):
+                foodFinalData[restaurant] += foodRawData[restaurant][i][0]
+                for flag in foodRawData[restaurant][i][1:]:
+                    if "vegan" in flag:
+                        foodFinalData[restaurant] += " V"
+                    if "gluten-free" in flag:
+                        foodFinalData[restaurant] += " G"
+                    if "lactose-free" in flag:
+                        foodFinalData[restaurant] += " L"
+                foodFinalData[restaurant] += "\n"
 
-copystr2 = []
+    return foodFinalData
 
-for i in copystr[1:]:
-    copystr2.append(i.split("("))
+restaurantsToDisplay = ["Chemicum", "Exactum", "Chemicum Opettajien ravintola"]
 
-print("Chemicum:")
-for i in copystr2:
-    print(i[0])
+htmlstr = gethtml("https://folio.kanttiinit.fi/fi/area/7")
+foodToDisplay = parseFoodData(htmlstr, restaurantsToDisplay)
+
+index = 0
+#print fetch time with the first restaurant name
+for restaurant in foodToDisplay.keys():
+    if index ==0:
+        print(f"{restaurant}: (haettu {strftime('%d-%m %H:%M', localtime())})")
+    else: 
+        print(f"{restaurant}:")
+
+    if foodToDisplay[restaurant] == "Ei ruokalistaa.":
+        print(foodToDisplay[restaurant])
+        print()
+    else:
+        print(foodToDisplay[restaurant])
+
+    index += 1
